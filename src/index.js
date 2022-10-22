4e7d66c0c3a0292325132d849da4244e6ab0da99
Kristina <davydovak99999@gmail.com>
2022-10-22 23:02:48 +0300

    HW#9

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden"
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
+
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on ", anchors[i]);
+        showDetals();
         setDetails(anchors[i]);
     })
+}
+function showDetals(){
+    mainClass.classList.remove("hidden");
+}
+function hideDetails()
+{
+mainClass.classList.add(HIDDEN);
 }
