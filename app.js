9df74110b61bda8d5f90f3eeff925ab5e534f466
Kristina <davydovak99999@gmail.com>
2022-10-29 23:50:35 +0300

    js-language hw#1

---
+++ app.js
@@ -0,0 +1,21 @@
+function sum(op1, op2){
+    let res = op1 + op2;
+    return res;
+}
+let op1 = 10;
+let op2 = 20
+console.log(sum(op1, op2))
+
+
+
+function sumDigits(number){
+    let res = 9+7+8;
+return res;
+}
+console.log(sumDigits(-978));
+
+function sumDigits1(number){
+    let res = 11+4+7;
+return res;
+}
+console.log(sumDigits1(1147));
