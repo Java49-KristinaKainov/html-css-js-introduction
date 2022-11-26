c7c73ffc040cfabd37d8fc3720af1075956e9edf
Kristina <davydovak99999@gmail.com>
2022-11-26 21:32:18 +0200

    cw 19

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-// function getAddressKey(){
-//     return "address";
-// }
-
-// const person= {id:123, name: 'Vasya', last_name: 'Ivanov',
-//  year: 2000, address: {city: 'Lod',street: 'Sokolov', app: 100}
-// };
-//  console.log(person[yearExp]);
-//  console.log(person.address.street);
-
-function displayOccurrences(strings){
-    const occurrences = {};
-    strings.forEach(element => {
-        if (occurrences[element]) {
-            occurrences[element] ++;
-        } else{
-            occurrences[element] = 1;
-        }
-    });
-     //console.log(Object.entries(occurrences)); 
-  const occurrencesAr = Object.entries(occurrences);
-  occurrencesAr.sort((e1, e2) => e2[1] - e1[1]);
-  console.log(occurrencesAr);
+function createEmployee(id, name, birthYear, salary){
+    return {id, name, birthYear, salary};
 }
+const empl1 = createEmployee(123, "Vasya", 2000, 10000);
+const empl2 = createEmployee(123, "Vasya", 2000, 10000);
+// console.log(empl1 == empl2);
 
-// const x = {};
-// const string = "abc";
-// x[string] = 1;
-// console.log(x);
-// x[string] ++;
-// console.log(x);
+function updateSalary(empl1, newSalary){
+    empl1.salary = newSalary;
+}
+updateSalary(empl1, 15000);
+console.log(empl1);
 
-const strings = ["a", "opr", "lmn", "abc", "lmn", 
-"abs", "lmn", "lmn", "abc", "a"];
-displayOccurrences(strings);
+function updateSalaryPrimitive(salary, newSalary){
+    salary = newSalary;
+}
+let salary = 10000;
+updateSalaryPrimitive(salary, 20000);
+console.log("salary", salary);
+
